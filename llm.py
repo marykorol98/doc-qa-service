a0a21dfd1faea1f6f05679073bbe44aad58b76ee
marykorol98 <mashak.1998@gmail.com>
2025-11-30 01:10:02 +0300

    added api

--- llm.py
+++ llm.py
@@ -1,9 +1,58 @@
-# llm.py
-from langchain_community.llms import HuggingFaceEndpoint
+from langchain_huggingface import HuggingFaceEndpoint
 import os
+import logging
+from dotenv import load_dotenv
 
-def get_llm():
-    return HuggingFaceEndpoint(
-        repo_id=os.getenv("LLM_REPO_ID"),
-        huggingfacehub_api_token=os.getenv("HF_API_TOKEN"),
-    )
+load_dotenv() 
+
+logger = logging.getLogger(__name__)
+MAX_LOG_LENGTH = 120
+
+
+class DocLLM:
+    def __init__(self):
+        self._model = HuggingFaceEndpoint(
+            repo_id=os.getenv("LLM_REPO_ID"),
+            huggingfacehub_api_token=os.getenv("HF_API_TOKEN"),
+            task="conversational"
+        )
+        self.context: dict[str, str] = {}
+
+        self.questions: dict[str, str] = {}
+
+    @property
+    def model(self):
+        return self._model
+
+    def load_context_from_doc(self, doc_id, doc_text: str):
+        self.context[doc_id] = doc_text
+        logger.info(
+            f"Saved context: {doc_text[:MAX_LOG_LENGTH] if len(doc_text) > MAX_LOG_LENGTH else doc_text}"
+        )
+
+    def prompt(self, context: str, question: str):
+        return f"""Контекст:
+        {context}
+
+        Вопрос: {question}
+
+        Ответь кратко и по делу.
+        """
+
+    def ask(self, q_id: str, file_id: str, question: str) -> str:
+        prompt = self.prompt(self.context.get(file_id, ""), question)
+
+        answer = self.model.invoke(prompt)
+        
+        logger.info(
+            f"Generated answer: {answer[:MAX_LOG_LENGTH] if len(answer) > MAX_LOG_LENGTH else answer}"
+        )
+
+        self.questions[q_id] = {
+            "file_id": file_id,
+            "question": question,
+            "status": "completed",
+            "answer": answer,
+        }
+
+        return answer
---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from uuid import uuid4
+import requests
+from llm import DocLLM
+
+app = FastAPI()
+
+llm = DocLLM()
+
+
+@app.post("/upload")
+async def upload_file(url: str) -> dict[str, str]:
+    file_id = str(uuid4())
+
+    # Загружаем текст Google Docs в plain text
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        return {"error": "failed to download url"}
+
+    llm.load_context_from_doc(file_id, resp.text)
+
+    return {"file_id": file_id}
+
+
+@app.post("/ask")
+async def ask_question(file_id: str, question: str) -> dict[str, str]:
+    q_id = str(uuid4())
+
+    llm.ask(q_id, file_id, question)
+
+    return {"question_id": q_id}
+
+
+@app.get("/answer")
+async def get_answer(q_id: str):
+    if q_id not in llm.questions:
+        # TODO: в этом кейсе тоже должен быть статус
+        return JSONResponse({"error": "question not found"}, status_code=404)
+
+    q = llm.questions[q_id]
+
+    return {
+        "status": q["status"],
+        "answer": q["answer"],
+    }
