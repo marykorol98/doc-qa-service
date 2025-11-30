f3a5c5c771194b34b25db1e2bb0333c54d058242
marykorol98 <mashak.1998@gmail.com>
2025-11-30 09:50:32 +0300

    built baseline model

--- llm.py
+++ llm.py
@@ -1,20 +1,31 @@
-from langchain_huggingface import HuggingFaceEndpoint
+import requests
+from huggingface_hub import InferenceClient
 import os
 import logging
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 
 logger = logging.getLogger(__name__)
-MAX_LOG_LENGTH = 120
 
 
 class DocLLM:
+    MAX_LOG_LENGTH = 120
+    MAX_CONTEXT_CHARS = 20000
+
     def __init__(self):
-        self._model = HuggingFaceEndpoint(
-            repo_id=os.getenv("LLM_REPO_ID"),
-            huggingfacehub_api_token=os.getenv("HF_API_TOKEN"),
-            task="conversational"
+        self.model_id = os.getenv("LLM_REPO_ID")
+        self.token = os.getenv("HF_API_TOKEN")
+
+        self.api_url = "https://router.huggingface.co/v1/chat/completions"
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+        }
+
+        self._model = InferenceClient(
+            model=os.getenv("LLM_REPO_ID"),
+            token=os.getenv("HF_API_TOKEN"),
         )
         self.context: dict[str, str] = {}
 
@@ -27,10 +38,12 @@ class DocLLM:
     def load_context_from_doc(self, doc_id, doc_text: str):
         self.context[doc_id] = doc_text
         logger.info(
-            f"Saved context: {doc_text[:MAX_LOG_LENGTH] if len(doc_text) > MAX_LOG_LENGTH else doc_text}"
+            f"Saved context: {doc_text[: self.MAX_LOG_LENGTH] if len(doc_text) > self.MAX_LOG_LENGTH else doc_text}"
         )
 
     def prompt(self, context: str, question: str):
+        context = context[: self.MAX_CONTEXT_CHARS]
+
         return f"""Контекст:
         {context}
 
@@ -42,10 +55,29 @@ class DocLLM:
     def ask(self, q_id: str, file_id: str, question: str) -> str:
         prompt = self.prompt(self.context.get(file_id, ""), question)
 
-        answer = self.model.invoke(prompt)
-        
+        payload = {
+            "model": self.model_id,
+            "messages": [{"role": "user", "content": prompt}],
+        }
+
+        response = requests.post(
+            self.api_url, headers=self.headers, json=payload, timeout=60
+        )
+        data = response.json()
+
+        print(data)
+
+        # Формат:
+        # {
+        #   "choices": [
+        #     {"message": {"role": "assistant", "content": "..."}}
+        #   ]
+        # }
+
+        answer = data["choices"][0]["message"]["content"]
+
         logger.info(
-            f"Generated answer: {answer[:MAX_LOG_LENGTH] if len(answer) > MAX_LOG_LENGTH else answer}"
+            f"Generated answer: {answer[: self.MAX_LOG_LENGTH] if len(answer) > self.MAX_LOG_LENGTH else answer}"
         )
 
         self.questions[q_id] = {
